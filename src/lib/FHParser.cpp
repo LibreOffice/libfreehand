c46951fb273b28e1a46987cd660408ceb6455b32
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-04-22 23:00:15 +0200

    Stub data reader functions

--- src/lib/FHParser.cpp
+++ src/lib/FHParser.cpp
@@ -115,4 +115,456 @@ void libfreehand::FHParser::parseData(WPXInputStream *input)
 {
 }
 
+void libvisio::FHParser::readAGDFont(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readAGDSelection(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readArrowPath(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readAttributeHolder(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBasicFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBasicLine(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBendFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBlock(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBrushList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBrush(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBrushStroke(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readBrushTip(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readCalligraphicStroke(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readCharacterFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readClipGroup(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readCollector(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readColor6(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readCompositePath(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readConeFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readConnectorLine(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readContentFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readContourFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readCustomProc(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readDataList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readData(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readDateTime(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readDuetFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readElement(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readElemList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readElemPropLst(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readEnvelope(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readExpandFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readExtrusion(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFHDocHeader(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFigure(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFileDescriptor(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFilterAttributeHolder(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWBevelFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWBlurFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWFeatherFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWGlowFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWShadowFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readFWSharpenFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readGradientMaskFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readGraphicStyle(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readGroup(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readGuides(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readHalftone(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readImageFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readImageImport(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readLayer(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readLensFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readLinearFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readLinePat(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readLineTable(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageDocMan(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageElement(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageLayerElement(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageLayerInstance(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageSymbolClass(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMasterPageSymbolInstance(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMDict(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMName(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMpObject(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMQuickDict(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMString(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMultiBlend(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readMultiColorList(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readNewBlend(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readNewContourFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readNewRadialFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readOpacityFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readOval(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readParagraph(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPath(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPathTextLineInfo(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPatternFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPatternLine(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPerspectiveEnvelope(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPerspectiveGrid(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPolygonFigure(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readProcedure(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPropLst(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readPSLine(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readRadialFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readRadialFillX(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readRaggedFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readRectangle(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSketchFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSpotColor6(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readStylePropLst(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSwfImport(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSymbolClass(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSymbolInstance(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readSymbolLibrary(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTabTable(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTaperedFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTaperedFillX(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTEffect(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTextBlok(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTextColumn(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTextInPath(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTFOnPath(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTileFill(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTintColor6(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTransformFilter(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readTString(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readUString(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readVDict(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readVMpObj(WPXInputStream * /* input */)
+{
+}
+
+void libvisio::FHParser::readXform(WPXInputStream * /* input */)
+{
+}
+
 /* vim:set shiftwidth=2 softtabstop=2 expandtab: */
