e995332521c0df01294b3567c70e7d688c319fa7
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-05-24 09:32:43 +0000

    avoid integer wrap in FHInternalStream::read bounds check
    Compute remaining bytes as "bufSize - pos" after a non-negative clamp on
    m_offset, then take the smaller of numBytes and remaining.
    
    Change-Id: Id61d0f20f2dbe2927527b2f74efdba606fbe8e87
    Reviewed-on: https://gerrit.libreoffice.org/c/libfreehand/+/205603
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- src/lib/FHInternalStream.cpp
+++ src/lib/FHInternalStream.cpp
@@ -97,12 +97,13 @@ const unsigned char *libfreehand::FHInternalStream::read(unsigned long numBytes,
   if (numBytes == 0)
     return nullptr;
 
-  unsigned numBytesToRead;
+  if (m_offset < 0)
+    return nullptr;
 
-  if ((m_offset+numBytes) < m_buffer.size())
-    numBytesToRead = numBytes;
-  else
-    numBytesToRead = m_buffer.size() - m_offset;
+  const unsigned long bufSize = m_buffer.size();
+  const unsigned long pos = static_cast<unsigned long>(m_offset);
+  const unsigned long remaining = pos < bufSize ? bufSize - pos : 0;
+  const unsigned long numBytesToRead = numBytes < remaining ? numBytes : remaining;
 
   numBytesRead = numBytesToRead; // about as paranoid as we can be..
 
